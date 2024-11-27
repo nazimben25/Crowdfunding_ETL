9034b5d7b8ae086de02dd99825c4217e9a9fcf74
SunilduthBaichoo <sunilduth.baichoo@gmail.com>
2024-11-27 15:57:40 -0500

    - comit the part3 (SQL)

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,59 @@
+﻿-- crowdfunding_db_schema.sql
+-- 
+
+
+CREATE TABLE   category   (
+      category_id   varchar(6)   NOT NULL,
+      category   varchar(20)   NOT NULL,
+    CONSTRAINT   pk_category   PRIMARY KEY (
+          category_id  
+     )
+);
+
+CREATE TABLE   subcategory   (
+      subcategory_id   varchar(10)   NOT NULL,
+      subcategory   varchar(30)   NOT NULL,
+    CONSTRAINT   pk_subcategory   PRIMARY KEY (
+          subcategory_id  
+     )
+);
+
+CREATE TABLE   contacts   (
+      contact_id   int   NOT NULL,
+      first_name   varchar(20)   NOT NULL,
+      last_name   varchar(20)   NOT NULL,
+      email   varchar(60)   NOT NULL,
+    CONSTRAINT   pk_contacts   PRIMARY KEY (
+          contact_id  
+     )
+);
+
+CREATE TABLE   compaign   (
+      cf_id   int   NOT NULL,
+      contact_id   int NOT NULL,
+      company_name   varchar(50)   NOT NULL,
+      description   varchar(60)   NOT NULL,
+      goal   float   NOT NULL,
+      pledged   float   NOT NULL,
+      outcome   varchar(20)   NOT NULL,
+      backers_count   int   NOT NULL,
+      country   varchar(5)   NOT NULL,
+      currency   varchar(5)   NOT NULL,
+      launch_date   date   NOT NULL,
+      end_date   date   NOT NULL,
+      category_id   varchar(6)  NOT NULL,
+      subcategory_id   varchar(10)  NOT NULL,
+    CONSTRAINT   pk_compaign   PRIMARY KEY (
+          cf_id  
+     )
+);
+
+
+SELECT * FROM category
+
+SELECT * FROM subcategory
+
+SELECT * FROM contacts
+
+SELECT * FROM compaign
+
